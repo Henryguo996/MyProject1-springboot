356c0014e548fced812e0563ca13f623c54e62d0
Henryguo996 <119644872+Henryguo996@users.noreply.github.com>
2025-05-13 13:45:36 +0800

    Init Spring Boot project

---
+++ myproject1-springboot/src/main/java/com/guohenry/myproject1springboot/Myproject1SpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.guohenry.myproject1springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myproject1SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Myproject1SpringbootApplication.class, args);
+    }
+
+}
---
+++ myproject1-springboot/src/test/java/com/guohenry/myproject1springboot/Myproject1SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guohenry.myproject1springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myproject1SpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
